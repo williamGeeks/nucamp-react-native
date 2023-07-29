fe59f8f5632ee73a7fd22dd4b38395a480a1a893
Will Weeks <willweeks@Wills-Air.attlocal.net>
2023-07-29 10:45:52 -0400

    Week 4 Workshop Assignment

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from "expo-image-picker";
 import { baseUrl } from "../shared/baseUrl";
 import logo from "../assets/images/logo.png";
+import * as ImageManipulator from "expo-image-manipulator";
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState("");
@@ -148,7 +149,34 @@ const RegisterTab = () => {
             });
             if (capturedImage.assets) {
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+                processImage(capturedImage.assets[0].uri);
+            }
+        }
+    };
+
+    const processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{ resize: { width: 400 } }],
+            { format: ImageManipulator.SaveFormat.PNG }
+        );
+        console.log(processedImage);
+        setImageUrl(processedImage.uri);
+    };
+
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === "granted") {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1],
+            });
+
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                processImage(capturedImage.assets[0].uri);
             }
         }
     };
@@ -163,6 +191,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title="Camera" onPress={getImageFromCamera} />
+                    <Button title="Gallery" onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder="Username"
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -232,6 +232,19 @@ const CustomDrawerContent = (props) => (
 const Main = () => {
     const dispatch = useDispatch();
 
+    const showNetInfo = async () => {
+        let connectionInfo = await NetInfo.fetch();
+        Platform.OS === "ios"
+            ? Alert.alert(
+                  "Initial Network Connectivity Type:",
+                  connectionInfo.type
+              )
+            : ToastAndroid.show(
+                  "Initial Network Connectivity Type: " + connectionInfo.type,
+                  ToastAndroid.LONG
+              );
+    };
+
     useEffect(() => {
         dispatch(fetchCampsites());
         dispatch(fetchPromotions());
@@ -240,18 +253,7 @@ const Main = () => {
     }, [dispatch]);
 
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
-            Platform.OS === "ios"
-                ? Alert.alert(
-                      "Initial Network Connectivity Type:",
-                      connectionInfo.type
-                  )
-                : ToastAndroid.show(
-                      "Initial Network Connectivity Type: " +
-                          connectionInfo.type,
-                      ToastAndroid.LONG
-                  );
-        });
+        showNetInfo();
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
